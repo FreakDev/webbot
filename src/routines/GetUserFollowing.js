be8fa4a7ee4271865ea4ba430358f3b0dc4c465a
Mathias Desloges <freakdev@gmail.com>
2019-09-26 21:06:51 -0400

    implement fetch following

---
+++ src/routines/GetUserFollowing.js
@@ -0,0 +1,59 @@
+import Routine from "./RoutineAbstract";
+
+export default class GetUserFollowing extends Routine {
+
+    async run (username) {
+        await this._gotoProfilePage(username)
+        await this._openFollowersList()
+        await this._fetchAllFollowers()
+        return await this._extractFollowersList()
+    }
+
+    async _gotoProfilePage (username) {
+        const userProfilePage = "https://www.instagram.com/" + username + "/";
+
+        return this._browser.currentPageUrl !== userProfilePage
+            ? await this._browser.goto(userProfilePage)
+            : Promise.resolve();
+    }
+
+    async _openFollowersList () {
+        const followersButtonSelector = "a[href$=\"following/\"]";
+
+        return await this._browser.click(followersButtonSelector);
+    }
+
+    async _fetchAllFollowers () {
+        // scroll into the followers list to load all
+        const followersListWrapper = "div[role=dialog] > div:nth-child(3)";
+
+        let followersListWrapperNode = await this._browser.getNodes(followersListWrapper);
+        await followersListWrapperNode[0].evaluate(node => {
+            return new Promise( (resolve ) => {
+                let scrollTop = -1, endReachedCount = 0, endReachedLimit = 5
+                const doScroll = () => {
+                    if (node.scrollTop !== scrollTop) {
+                        scrollTop = node.scrollTop;
+                        node.scrollTo(0, node.scrollTop + 20);
+                    }
+                    else {
+                        endReachedCount++;
+                        if (endReachedCount > endReachedLimit) {
+                            resolve(true);
+                            return;
+                        }
+                    }
+                    setTimeout(doScroll, 150);
+                }
+                setTimeout(doScroll, 150)
+            } )
+        });
+    }
+
+    async _extractFollowersList () {
+        const followersListItemSelector = "div[role=dialog] ul li a:nth-child(1)";
+
+        return await this._browser.getText(followersListItemSelector)
+    }
+
+}
