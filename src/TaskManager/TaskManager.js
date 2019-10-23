655003b8be94a5877e91ec0c14a99580d709912a
Mathias Desloges <freakdev@gmail.com>
2019-10-23 00:21:26 -0400

    implement no spam delay

--- src/TaskManager/TaskManager.js
+++ src/TaskManager/TaskManager.js
@@ -1,5 +1,14 @@
+import { resolve } from "path"
 
 export default class TaskManager {
+
+    _lastExecution = {}
+
+    _minDelay = {
+        "like-photo": 2,
+        "follow-user": 20,
+        "unfollow-user": 10
+    }
 
     constructor() {
         this._stack = []
@@ -9,9 +18,13 @@ export default class TaskManager {
         
         if (this._stack.length)
             setTimeout(async () => {
-                await this._pop()
-                this.run(Math.random() * 5000)
+                await this._pop();
+                const delay = Math.random() * 1000 + 1000;
+                console.log("applying a random delay of " + delay + "ms")
+                this.run(delay)
             }, timeout)
+        else 
+            console.log("JOB DONE !")
     }
 
     add (task) {
@@ -19,11 +32,41 @@ export default class TaskManager {
     }
 
     async _pop () {
-        const task = this._stack.shift()
+        return new Promise(async (resolve, reject) => {
+            const task = this._stack.shift()
+            
+            const delay = this._calculateNoSpamDelay(task.name)
+            if (delay && task.skippable) {
+                console.log("skip ", task.name);
+                return resolve()
+            }
 
-        console.log(task.name)
+            console.log("applying a delay of " + delay + "ms before " + task.name)
+            setTimeout(async () => {
+                const result = await task.run()
+                resolve(result)
+            }, delay)
+        }) 
+    }
 
-        await task.run()
+    _calculateNoSpamDelay (name) {
+        let delay = 0;
+        const now = (new Date()).getMilliseconds();
+
+        if (this._lastExecution[name]) {
+            const lastTime = (new Date(this._lastExecution[name].lastExecTime)).getMilliseconds(),
+                delta = now - lastTime;
+
+            if (delta < (this._minDelay[name] * 1000)) {
+                delay = (Math.random() * (this._minDelay[name] * 1000)) + ((this._minDelay[name] * 1000) - delta);
+            }
+        } else {
+            this._lastExecution[name] = { count: 0, lastExecTime: now };
+        }
+        this._lastExecution[name].count++;
+        this._lastExecution[name].lastExecTime = now;
+
+        return delay;
     }
 
 }
